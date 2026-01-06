2dc193aa23efa3b4f811de55df37a58cd11a6e8d
Sumit Srivastava <sumitsrivastava074@gmail.com>
2026-01-06 11:25:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,6 @@ def submit():
     return jsonify({'message': new_message})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    init.db()
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
