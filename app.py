bb3693ae3dbcf17334daf95800025f2aa4f5cc7e
Sumit Srivastava <sumitsrivastava074@gmail.com>
2026-01-06 11:21:50 +0530

    Run app without debug mode
    Removed debug mode from app.run()

--- app.py
+++ app.py
@@ -43,6 +43,5 @@ def submit():
     return jsonify({'message': new_message})
 
 if __name__ == '__main__':
-    init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
